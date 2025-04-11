a5d78b5eb3214c527750c6ffa94abad40a56ac3f
Rahul Raj <rahulraj1417@gmail.com>
2025-04-11 17:29:41 +0530

    fix: _

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -1,12 +1,9 @@
 export async function handler(event) {
   const { path, queryStringParameters, httpMethod, headers, body } = event;
 
-  const targetUrl =
-    "https://torrentio.strem.fun/torbox=5aff16a1-016c-4e38-abf5-8944cc76b5b7/stream/series/tt2085059%3A7%3A2.json";
-  // const targetUrl = `https://your-api-domain.com${event.rawUrl.replace(
-  //   /^.*\/api/,
-  //   ""
-  // )}`;
+  const targetUrl = "https://torrentio.strem.fun" + path.replace(/\/api/, "");
+
+  console.log(event);
 
   const res = await fetch(targetUrl, {
     method: httpMethod,
